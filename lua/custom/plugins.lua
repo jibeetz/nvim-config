b8aa19ba3128a32db4b036bc962f95f8e02a2842
Jean-Baptiste Bernus <jbernus@gmail.com>
2023-12-29 21:49:29 +0100

    save config

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,11 @@
+local plugins = {
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function ()
+      return require "custom.configs.null-ls"
+    end
+  }
+}
+
+return plugins
